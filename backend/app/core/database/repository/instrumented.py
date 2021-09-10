c84c670084d23abce0bc6e6e3b712890c72494f2
Davide Pedranz <dp@bendingspoons.com>
2021-09-10 17:29:45 +0200

    feat: add instrumentation to backend

--- backend/app/core/database/repository/instrumented.py
+++ backend/app/core/database/repository/instrumented.py
@@ -1,11 +1,15 @@
 from typing import Optional, Tuple
 from uuid import UUID
 
+from prometheus_client import Histogram
+
 from app.core.database.repository.base import Repository
 from app.core.todo import Todo
 
 
-# TODO: measure the execution time of the different database operations
+# TASK: measure the execution time of the different database operations
+# SOLUTION: we create a new `Histogram` object and wrap each call with the `.time()` context
+#           manager to measure the execution time; we use a label to identify the operation
 class InstrumentedRepository(Repository):
     """
     Prometheus-instrumented decorator for a concrete implementation of Repository.
@@ -16,29 +20,44 @@ class InstrumentedRepository(Repository):
     ```
     """
 
+    _HISTOGRAM = Histogram(
+        name="app_query_duration",
+        unit="seconds",
+        documentation="Time required to handle a query to the Todos repository",
+        labelnames=("query",),
+    )
+
     def __init__(self, repository: Repository):
         self._repository = repository
 
     async def init(self) -> None:
-        await self._repository.init()
+        with self._HISTOGRAM.labels(query="init").time():
+            await self._repository.init()
 
     async def get(self, id_: UUID) -> Optional[Todo]:
-        return await self._repository.get(id_=id_)
+        with self._HISTOGRAM.labels(query="get").time():
+            return await self._repository.get(id_=id_)
 
     async def list(self) -> Tuple[Todo, ...]:
-        return await self._repository.list()
+        with self._HISTOGRAM.labels(query="list").time():
+            return await self._repository.list()
 
     async def insert(self, text: str) -> UUID:
-        return await self._repository.insert(text=text)
+        with self._HISTOGRAM.labels(query="insert").time():
+            return await self._repository.insert(text=text)
 
     async def edit_text(self, id_: UUID, text: str) -> bool:
-        return await self._repository.edit_text(id_=id_, text=text)
+        with self._HISTOGRAM.labels(query="edit_text").time():
+            return await self._repository.edit_text(id_=id_, text=text)
 
     async def activate(self, id_: UUID) -> bool:
-        return await self._repository.activate(id_=id_)
+        with self._HISTOGRAM.labels(query="activate").time():
+            return await self._repository.activate(id_=id_)
 
     async def deactivate(self, id_: UUID) -> bool:
-        return await self._repository.deactivate(id_=id_)
+        with self._HISTOGRAM.labels(query="deactivate").time():
+            return await self._repository.deactivate(id_=id_)
 
     async def delete(self, id_: UUID) -> bool:
-        return await self._repository.delete(id_=id_)
+        with self._HISTOGRAM.labels(query="delete").time():
+            return await self._repository.delete(id_=id_)
